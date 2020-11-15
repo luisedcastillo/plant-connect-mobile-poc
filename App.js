983f217a93610217efe12ca5d1de4499227f908e
Luis Castillo <luised.castillo@gmail.com>
2020-11-15 15:53:07 -0600

    Add react native paper package

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Plant Connect PoC</Text>
+      <Text>Hello World!! Gabriel....</Text>
+      <Button mode="text">
+        Press me!
+      </Button>
       <StatusBar style="auto" />
     </View>
   );
